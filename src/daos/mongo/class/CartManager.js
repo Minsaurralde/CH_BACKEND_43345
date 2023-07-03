b61abb4933db3c84ce9cddcb714fb2fdb1a7f80a
Mariela Insaurralde <minsaurralde@teco.com.ar>
2023-07-03 02:56:36 -0300

    feat: adding new endpoints put and delete to cart manager

--- src/daos/mongo/class/CartManager.js
+++ src/daos/mongo/class/CartManager.js
@@ -46,6 +46,78 @@ class CartManager {
 
     return;
   };
+
+  deleteProductCart = async (cartID, prodID) => {
+    if (!cartID || !prodID) {
+      throw new Error("deleteProductCart responde: faltan datos obligatorios");
+    }
+
+    const cart = await this.getCartsById(cartID);
+    const foundProduct = cart.products.find((p) => p.product == prodID);
+
+    if (!foundProduct) {
+      throw new Error(
+        "deleteProductCart responde: el carrito no contiene ese producto"
+      );
+    } else {
+      cart.products.pull(foundProduct._id);
+      await cart.save();
+    }
+
+    return;
+  };
+
+  deleteAllProductsCart = async (cartID) => {
+    if (!cartID) {
+      throw new Error("deleteProductCart responde: faltan datos obligatorios");
+    }
+
+    const cart = await this.getCartsById(cartID);
+
+    cart.products = [];
+    await cart.save();
+
+    return;
+  };
+
+  updateProductCart = async (cartID, prodID, Qty) => {
+    if (!cartID || !prodID || !Qty) {
+      throw new Error("addProductCart responde: faltan datos obligatorios");
+    }
+
+    const cart = await this.getCartsById(cartID);
+
+    const prodList = cart.products;
+    console.log(prodList);
+
+    const existsProd = prodList.findIndex(
+      (el) => el.product.toString() === prodID
+    );
+
+    if (existsProd == -1) {
+      cart.products.push({ product: prodID, quantity: Qty }); // si no existe lo creo
+    } else {
+      prodList[existsProd].quantity = Qty; // si existe, piso el dato
+    }
+    cart.save();
+
+    return;
+  };
+
+  updateAllProductsCart = async (cartID, newProducts) => {
+    if (!cartID || !newProducts) {
+      throw new Error("addProductCart responde: faltan datos obligatorios");
+    }
+
+    const cart = await this.getCartsById(cartID);
+    const prodList = cart.products;
+    console.log(prodList);
+
+    prodList = newProducts; // en la consigna no esta especificado el formato - preguntar
+    cart.save();
+
+    return;
+  };
 }
 
 export default CartManager;
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -68,4 +68,75 @@ router.post("/:cid/product/:pid", async (req, res) => {
   }
 });
 
+// Debe eliminar del carrito el producto seleccionado.
+router.delete("/:cid/product/:pid", async (req, res) => {
+  const cartId = req.params.cid;
+  const prodId = req.params.pid;
+
+  //creo instancia de la clase
+  const instancia2 = new CartManager();
+
+  //inserto los datos
+  try {
+    await instancia2.deleteProductCart(cartId, prodId); // 2 - proceso el borrado
+    res.status(200).send({ exito: "se elimino con exito" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+// Debe eliminar del carrito todos los productos
+router.delete("/:cid", async (req, res) => {
+  const cartId = req.params.cid;
+
+  //creo instancia de la clase
+  const instancia2 = new CartManager();
+
+  //inserto los datos
+  try {
+    await instancia2.deleteAllProductsCart(cartId); // 2 - proceso el borrado
+    res.status(200).send({ exito: "los productos se eliminaron con exito" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+// Debe poder actualizar SÓLO la cantidad de ejemplares del producto por cualquier cantidad pasada desde req.body
+router.put("/:cid/product/:pid", async (req, res) => {
+  const cartId = req.params.cid;
+  const prodId = req.params.pid;
+  const qty = req.body.qty;
+
+  //creo instancia de la clase
+  const instancia1 = new ProductManager();
+  const instancia2 = new CartManager();
+
+  //inserto los datos
+  try {
+    await instancia1.getProductById(prodId); // 1 - valido que exista el prodId
+    await instancia2.updateProductCart(cartId, prodId, qty); // 2 - proceso el update
+    res.status(200).send({ exito: "se actualizo con exito" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+// Debe actualizar el carrito con un arreglo de productos con el formato especificado
+router.put("/:cid", async (req, res) => {
+  const cartId = req.params.cid;
+  const newProducts = req.body;
+
+  //creo instancia de la clase
+  const instancia1 = new ProductManager();
+  const instancia2 = new CartManager();
+
+  //inserto los datos
+  try {
+    await instancia2.updateAllProductsCart(cartId, newProducts); // 2 - proceso el update
+    res.status(200).send({ exito: "se actualizo con exito" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
 export default router;
